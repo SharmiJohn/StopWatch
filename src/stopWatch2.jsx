430c4cc3e55ab90cb1c40faca0afbb273db9a0cb
SharmiJohn <1422000joselvi@gmail.com>
2024-05-10 22:17:17 +0530

    second

---
+++ src/stopWatch2.jsx
@@ -0,0 +1,42 @@
+import React ,{useEffect,useState}from 'react'
+
+function StopWatch2() {
+    const[timer,settimer]=useState(false);
+    const[minute,setminute]=useState(0);
+    const[sec,setsec]=useState(0);
+    const handleReset=()=>{
+        settimer(false);
+        setminute(0);
+        setsec(0);
+    }
+    const handleClick=()=>{
+        settimer((prevstate)=>!prevstate)
+    }
+    useEffect(()=>{
+        let intervalId;
+        if(timer===true){
+            intervalId=setInterval(()=>{
+                if(sec>60){
+                  setminute((prev)=>prev+1);
+                  setsec(0);
+                }
+                else{
+                    setsec((prev)=>prev+1);
+                }
+            },1000)
+           
+        }
+        console.log(intervalId)
+        return()=>{
+            clearInterval(intervalId)
+        }
+    },[timer,sec])
+  return (
+    <div><h2>Stopwatch</h2>
+    <p>Timer:{minute}:{sec>10?(sec):("0"+sec)}</p>
+    <button onClick={()=>handleClick()}>{timer?"Stop":"Start"}</button>
+    <button onClick={()=>handleReset()}>Reset</button></div>
+  )
+}
+
+export default StopWatch2
