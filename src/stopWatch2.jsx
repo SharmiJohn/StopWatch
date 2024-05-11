2617bef1e4192acf8a30eda50a26e30ed3385f47
SharmiJohn <1422000joselvi@gmail.com>
2024-05-11 14:22:30 +0530

    final

--- src/stopWatch2.jsx
+++ src/stopWatch2.jsx
@@ -1,42 +1,42 @@
-import React ,{useEffect,useState}from 'react'
+import React, { useEffect, useState } from "react";
 
 function StopWatch2() {
-    const[timer,settimer]=useState(false);
-    const[minute,setminute]=useState(0);
-    const[sec,setsec]=useState(0);
-    const handleReset=()=>{
-        settimer(false);
-        setminute(0);
-        setsec(0);
+  const [timer, settimer] = useState(false);
+  const [time, settime] = useState(0);
+  const handleReset = () => {
+    settimer(false);
+    settime(0);
+  };
+  const handleClick = () => {
+    settimer((prev) => !prev);
+  };
+  useEffect(() => {
+    let intervalId;
+    if (timer === true) {
+      intervalId = setInterval(() => {
+        settime((prev) => prev + 1);
+      });
     }
-    const handleClick=()=>{
-        settimer((prev)=>!prev)
-    }
-    useEffect(()=>{
-        let intervalId;
-        if(timer===true){
-            intervalId=setInterval(()=>{
-                if(sec>58){
-                  setminute((prev)=>prev+1);
-                  setsec(0);
-                }
-                else{
-                    setsec((prev)=>prev+1);
-                }
-            },1000)
-           
-        }
-        console.log(intervalId)
-        return()=>{
-            clearInterval(intervalId)
-        }
-    },[timer,sec])
+    console.log(intervalId);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [timer]);
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+
+    const remainingSeconds = seconds % 60;
+
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  };
   return (
-    <div><h2>Stopwatch</h2>
-    <p>Timer:{minute}:{sec>9?(sec):("0"+sec)}</p>
-    <button onClick={()=>handleClick()}>{timer?"Stop":"Start"}</button>
-    <button onClick={()=>handleReset()}>Reset</button></div>
-  )
+    <div>
+      <h2>Stopwatch</h2>
+      <p>Timer:{formatTime(time)}</p>
+      <button onClick={() => handleClick()}>{timer ? "Stop" : "Start"}</button>
+      <button onClick={() => handleReset()}>Reset</button>
+    </div>
+  );
 }
 
-export default StopWatch2
+export default StopWatch2;
